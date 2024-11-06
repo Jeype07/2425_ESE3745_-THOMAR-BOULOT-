a241cb78d49ff0a207494067e733d6bcffaf77e0
Jeype07 <131168989+Jeype07@users.noreply.github.com>
2024-11-06 17:32:10 +0100

    add 4 pwm

--- NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
+++ NUCLEO-G474RET6-Inverter_Pinout/Core/Src/main.c
@@ -96,6 +96,14 @@ int main(void)
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
 	Shell_Init();
+	HAL_TIM_PWM_Start( &htim1, TIM_CHANNEL_1 );
+	HAL_TIMEx_PWMN_Start( &htim1, TIM_CHANNEL_1 );
+
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+    HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
+
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 638 );
+	__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 638 );
   /* USER CODE END 2 */
 
   /* Infinite loop */
